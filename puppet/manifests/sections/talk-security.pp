9d52701792e41cb0b02cdc5bb5f7fe030df88099
Michael D Adams <michael.d.adams@gmail.com>
2014-03-28 17:35:39 -0700

    security import

--- puppet/manifests/sections/talk-security.pp
+++ puppet/manifests/sections/talk-security.pp
@@ -1,8 +1,24 @@
 class { 'nodejs':
   version => 'stable',
   make_install => false,
-}
+} ->
 
 package { 'phantomjs':
   provider => npm
+}
+
+mysql::grant { 'security':
+  mysql_db         => 'security',
+  mysql_user       => 'username_here',
+  mysql_password   => 'password_here',
+  mysql_privileges => 'ALL',
+  mysql_host       => 'localhost',
+} ->
+
+mysql::queryfile { 'security.sql':
+  mysql_file       => '/srv/www/security/.db/security.sql',
+  mysql_db         => 'security',
+  mysql_user       => 'username_here',
+  mysql_password   => 'password_here',
+  mysql_host       => 'localhost',
 }
