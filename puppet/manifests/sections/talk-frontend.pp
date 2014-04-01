0da6a4b27462e60ea9b3b76cc53acf3732a86bb7
Beau Lebens <beau@dentedreality.com.au>
2014-03-31 17:03:53 -0700

    Frontend: Fix path to SQL file.

--- puppet/manifests/sections/talk-frontend.pp
+++ puppet/manifests/sections/talk-frontend.pp
@@ -7,7 +7,7 @@ mysql::grant { 'frontend':
 } ->
 
 mysql::queryfile { 'db.sql':
-  mysql_file       => '/srv/www/security/.db/db.sql',
+  mysql_file       => '/srv/www/front-end-performance/.db/db.sql',
   mysql_db         => 'frontend',
   mysql_user       => 'username_here',
   mysql_password   => 'password_here',
